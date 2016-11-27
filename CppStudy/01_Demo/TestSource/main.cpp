06acd65f582fc5bcb174413b4bfd94161c6f5be1
KamZhang <kamzhang123@163.com>
2016-11-27 20:03:39 +0800

    change 01_Demo

--- CppStudy/01_Demo/TestSource/main.cpp
+++ CppStudy/01_Demo/TestSource/main.cpp
@@ -5,21 +5,37 @@
 #include "base/BaseTypes.h"
 #include "GeneralFunction.h"
 using namespace std;
-#define GTEST_MODE  SWITCH_ON
-UCHAR b[256] = {0x01,0x23,0x45,0x56};
+#define GTEST_MODE  SWITCH_OFF
+#define LogPrintf(a) cout<<(a)<<'\n'
+
+struct fruit
+{
+	virtual void eat(){LogPrintf("eat fruit");}
+};
+struct apple : fruit
+{
+	void eat(){LogPrintf("eat apple");}
+};
+struct orange : fruit
+{
+	
+};
 int main(int argc, char **argv)
 {
 #if (SWITCH_ON == GTEST_MODE)
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();	
 #else
-    WORD32 x = 0xabcd1234;
-    WORD32 a[6] = {0x00112233,0x44556677,0x8899aabb,0xccddeeff,0x01234567,0x89abcdef};
-    
-    MemoryPrint(x);
-    MemoryPrint(a);
-    MemoryPrint(a[6]);
-    MemoryPrint(b);
+    fruit f;
+	apple app;
+	orange ora;
+    MemoryPrint(f);
+	MemoryPrint(app);
+	MemoryPrint(ora);
+
+	f.eat();
+	app.eat();
+	ora.eat();
 #endif
 }
 
